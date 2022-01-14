a751363e63e66b73f1481a814eea5c2832b83e4d
alaliyev <88936505+alaliyev@users.noreply.github.com>
2022-01-14 23:42:46 +0400

    Add files via upload

---
+++ ASP.Task1/ASP.Task1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Task1.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index() { 
+         return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
